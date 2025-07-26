53f8e00ac27b533a3545158cab8e6855c38bb249
GigabiteStudios <spencer.conlon3@gmail.com>
2025-07-26 15:07:41 -0500

    Adds package manager infrastructure
    Initializes the package manager classes, providing
    a foundation for handling dependencies.

---
+++ src/utils/LumenitePackageManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by spenc on 7/26/2025.
+//
+
+#include "LumenitePackageManager.h"
---
+++ src/utils/LumenitePackageManager.h
@@ -0,0 +1,16 @@
+//
+// Created by spenc on 7/26/2025.
+//
+
+#ifndef LUMENITEPACKAGEMANAGER_H
+#define LUMENITEPACKAGEMANAGER_H
+
+
+
+class LumenitePackageManager {
+
+};
+
+
+
+#endif //LUMENITEPACKAGEMANAGER_H
